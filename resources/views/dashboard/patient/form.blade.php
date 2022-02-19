27f282227161664df1aafcc15d2102b45c046a74
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-02-19 14:52:03 +0100

    Added enums and finished patient create form

--- resources/views/dashboard/patient/form.blade.php
+++ resources/views/dashboard/patient/form.blade.php
@@ -10,7 +10,8 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="my-2 mx-4">
 
-                    <form class="space-y-8 divide-y divide-gray-200">
+                    <form method="POST" action="{{ route('patients.store') }}" class="space-y-8 divide-y divide-gray-200">
+                        @csrf
                         <div class="space-y-8 divide-y divide-gray-200">
                             <div class="pt-2">
                                 <div>
@@ -19,91 +20,75 @@
                                 </div>
                                 <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
                                     <div class="sm:col-span-3">
-                                        <label for="first-name" class="block text-sm font-medium text-gray-700"> First
-                                            name </label>
+                                        <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                                         <div class="mt-1">
-                                            <input type="text" name="first-name" id="first-name"
+                                            <input type="text" name="name" id="name"
                                                    autocomplete="given-name"
                                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
 
                                     <div class="sm:col-span-3">
-                                        <label for="last-name" class="block text-sm font-medium text-gray-700"> Last
-                                            name </label>
+                                        <label for="date_of_birth" class="block text-sm font-medium text-gray-700"> Date of birth </label>
                                         <div class="mt-1">
-                                            <input type="text" name="last-name" id="last-name"
-                                                   autocomplete="family-name"
-                                                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
-
-                                    <div class="sm:col-span-4">
-                                        <label for="email" class="block text-sm font-medium text-gray-700"> Email
-                                            address </label>
-                                        <div class="mt-1">
-                                            <input id="email" name="email" type="email" autocomplete="email"
+                                            <input type="date" name="date_of_birth" id="date_of_birth"
+                                                   autocomplete="date_of_birth"
                                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
 
                                     <div class="sm:col-span-3">
-                                        <label for="country" class="block text-sm font-medium text-gray-700">
-                                            Country </label>
+                                        <label for="language" class="block text-sm font-medium text-gray-700">Language</label>
                                         <div class="mt-1">
-                                            <select id="country" name="country" autocomplete="country-name"
-                                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
-                                                <option>United States</option>
-                                                <option>Canada</option>
-                                                <option>Mexico</option>
+                                            <select id="language" name="language" autocomplete="language-name"
+                                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md uppercase">
+                                                @foreach(\App\Enums\LanguageEnum::cases() as $case)
+                                                    <option value="{{ $case->value }}" @if($case->value == old('language')) selected @endif>{{ $case->value }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                     </div>
 
-                                    <div class="sm:col-span-6">
-                                        <label for="street-address" class="block text-sm font-medium text-gray-700">
-                                            Street address </label>
+                                    <div class="sm:col-span-3">
+                                        <label for="gender" class="block text-sm font-medium text-gray-700">Gender</label>
                                         <div class="mt-1">
-                                            <input type="text" name="street-address" id="street-address"
-                                                   autocomplete="street-address"
-                                                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                            <select id="gender" name="gender" autocomplete="gender"
+                                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md capitalize">
+                                                @foreach(\App\Enums\GenderEnum::cases() as $case)
+                                                    <option value="{{ $case->value }}" @if($case->value == old('gender')) selected @endif>{{ $case->value }}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
                                     </div>
 
-                                    <div class="sm:col-span-2">
-                                        <label for="city" class="block text-sm font-medium text-gray-700"> City </label>
+                                    <div class="sm:col-span-3">
+                                        <label for="relation" class="block text-sm font-medium text-gray-700">Relation</label>
                                         <div class="mt-1">
-                                            <input type="text" name="city" id="city" autocomplete="address-level2"
-                                                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                            <select id="relation" name="relation"
+                                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md capitalize">
+                                                @foreach(\App\Enums\RelationEnum::cases() as $case)
+                                                    <option value="{{ $case->value }}" @if($case->value == old('relation')) selected @endif>{{ $case->value }}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
                                     </div>
 
-                                    <div class="sm:col-span-2">
-                                        <label for="region" class="block text-sm font-medium text-gray-700"> State /
-                                            Province </label>
+                                    <div class="sm:col-span-3">
+                                        <label for="color" class="block text-sm font-medium text-gray-700"> Color </label>
                                         <div class="mt-1">
-                                            <input type="text" name="region" id="region" autocomplete="address-level1"
-                                                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                            <input type="color" name="color" id="color"
+                                                   class="shadow-sm w-1/4 h-10 rounded focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                                         </div>
                                     </div>
 
-                                    <div class="sm:col-span-2">
-                                        <label for="postal-code" class="block text-sm font-medium text-gray-700"> ZIP /
-                                            Postal code </label>
-                                        <div class="mt-1">
-                                            <input type="text" name="postal-code" id="postal-code"
-                                                   autocomplete="postal-code"
-                                                   class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                             <div class="pt-5 pb-3">
                                 <div class="flex justify-end">
-                                    <button type="button"
+                                    <a href="{{ route('patients.index') }}"
                                             class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         Cancel
-                                    </button>
+                                    </a>
                                     <button type="submit"
                                             class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         Save
