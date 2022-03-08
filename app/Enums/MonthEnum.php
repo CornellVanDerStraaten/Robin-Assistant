869ca18e2ebef9e46c390c2c6d859a8a21cb762c
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-03-08 12:42:23 +0100

    Create MonthEnum.php

---
+++ app/Enums/MonthEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum MonthEnum: int
+{
+    case Januari = 1;
+    case Februari = 2;
+    case Maart = 3;
+    case April = 4;
+    case Mei = 5;
+    case Juni = 6;
+    case Juli = 7;
+    case Augustus = 8;
+    case September = 9;
+    case Oktober = 10;
+    case November = 11;
+    case December = 12;
+}
