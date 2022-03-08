2274488f181a1285145748e4fe01818ac663de08
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-03-08 12:42:47 +0100

    Send dateArray to every supervisor layout

--- app/View/Components/SupervisorsLayout.php
+++ app/View/Components/SupervisorsLayout.php
@@ -9,10 +9,12 @@ class SupervisorsLayout extends Component
     /**
      * Get the view / contents that represents the component.
      *
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function render()
     {
-        return view('layouts.supervisors');
+        return view('layouts.supervisors', [
+            'dateArray' => now()->toArray(),
+        ]);
     }
 }
